2ff82dd1ae58f1c52a0081ef2a170ecc3d925563
robertomc2007 <robertomc2007@gmail.com>
2016-08-17 15:38:44 -0300

    Update Transmisor.ino

--- TESTINICIAL/Transmisor.ino
+++ TESTINICIAL/Transmisor.ino
@@ -1,15 +1,18 @@
-// ask_transmitter
+// Transmisor.ino
 // -*- mode: C++ -*-
 // Simple example of how to use RadioHead to transmit messages
 // with a simple ASK transmitter in a very simple way.
 // Implements a simplex (one-way) transmitter with an TX-C1 module
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
+
 RH_ASK driver;
 
 void setup()
 {
     Serial.begin(115200);   // Debugging only
+    
+    Serial.println("Transmisor.ino");
     
     pinMode(13, OUTPUT);
     digitalWrite(13, LOW);
@@ -23,8 +26,9 @@ void setup()
 
 void loop()
 {
+    delay(2000);
     const char *msg = "Esta es la voz de los marcianos...";
     driver.send((uint8_t *)msg, strlen(msg));
     driver.waitPacketSent();
-    delay(2000);
+    Serial.println("Enviado");
 }
