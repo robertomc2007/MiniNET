a97776501cd6255fccd44b5225ee37832122fdbf
robertomc2007 <robertomc2007@gmail.com>
2016-11-09 18:40:33 -0300

    Add files via upload

---
+++ MAESTRO-ESCLAVO/RH_MASTER_TEST_V3/RH_MASTER_TEST_V3.ino
@@ -0,0 +1,249 @@
+// RHMASTER
+// -*- mode: C++ -*-
+// Simple example of how to use RadioHead to transmit messages
+// with a simple ASK transmitter in a very simple way.
+// Implements a simplex (one-way) transmitter with an TX-C1 module
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
+#include <RBD_Timer.h> // https://github.com/alextaujenis/RBD_Timer
+
+#define MAESTRO 0
+
+RH_ASK driver(2400, 5, 4);
+RBD::Timer timer, timersend;
+
+struct dataStruct {
+  byte tipo; //1=interroga 2=responde 3= ping 4=pong
+  byte to;
+  byte from;
+  byte maxnode;
+  byte hope;
+  float dato;
+  unsigned long contador;
+} DatosTipo;
+
+byte buf[RH_ASK_MAX_MESSAGE_LEN];
+byte buflen = sizeof(buf);
+byte tx_buf[RH_ASK_MAX_MESSAGE_LEN + 100] = {0};
+
+boolean dump = false;
+boolean interroga = false;
+boolean muestra = false;
+int maxesclavo = 5;
+byte esclavo = 1;
+unsigned long tiempo = 0;
+String salida = "";
+
+void setup()
+{
+  Serial.begin(115200);   // Debugging only
+
+  Serial.println(F("RH MASTER"));
+  Serial.println(RH_ASK_MAX_MESSAGE_LEN);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+
+  if (!driver.init())
+  {
+    Serial.println(F("FALLA INICIO DRIVER"));
+    digitalWrite(13, HIGH);
+  }
+  timer.setTimeout(3500);
+  timersend.setTimeout(20000);
+  timer.stop();
+  timersend.stop();
+  Serial.println(F("DETIENE TIMERS"));
+}
+
+void loop()
+{
+
+  //Comandos via puerto serie
+  if ( Serial.available()) {
+
+    char ch = Serial.read();
+    switch (ch) {
+      //    case '0'...'9':
+      //      //v = v * 10 + ch - '0';
+      //      v = ch - '0';
+      //      break;
+      case 'd': //vuelco
+        {
+          dump = !dump;
+          break;
+        }
+      case 'm': //muestra preliminares
+        {
+          muestra = !muestra;
+          break;
+        }
+      case 't': //timers interroga
+        {
+          interroga = !interroga;
+          if (interroga) {
+            timer.stop();
+            timersend.stop();
+            Serial.println(F("DETIENE TIMERS"));
+          } else {
+            timer.restart();
+            Serial.println(F("INICIA TIMERS"));
+          }
+          break;
+        }
+      //nro de esclavos
+      case 'e':
+        {
+          maxesclavo = Serial.parseInt();
+          if (maxesclavo <= 0 || maxesclavo > 8) {
+            maxesclavo = 1;
+          }
+          break;
+        }
+      //interroga
+      case 'i':
+        {
+          int destino = Serial.parseInt();
+          DatosTipo.tipo = 1; // Mensaje de interrogacion
+          DatosTipo.to = (byte)destino;
+          DatosTipo.contador = millis();
+          SendDatos();
+          break;
+        }
+
+      //ping
+      case 'p':
+        {
+          int destino = Serial.parseInt();
+          DatosTipo.tipo = 3;
+          DatosTipo.to = (byte)destino;
+          DatosTipo.contador = millis();
+          SendDatos();
+          break;
+        }
+      //help
+      case '?':
+        {
+          Serial.println(F("\n\rs: estado y parametros\n\rk: clave TS\n\rn: #canal TS\n\ri: nombre WiFi\n\rp: clave WiFi"));
+          Serial.println(F("w: conecta WiFi\n\ra: desconecta y lista redes\n\ru: activa/des upload\n\rc: #campo TS (1-7)\n\rv: valor en campo TS (float)"));
+          Serial.println(F("r: reset\n\rf: guarda config.json\n\rd: activa/des debug\n\r"));
+          break;
+        }
+    }
+  }
+
+  buflen = sizeof(buf);
+  if (driver.recv(buf, &buflen)) // Non-blocking
+  { // Mensaje con checksum correcto recibido.
+
+    // Mensaje con checksum recibido.
+    memcpy(&DatosTipo, buf, sizeof(DatosTipo));
+
+    if (dump) {
+      Serial.println("***");
+      driver.printBuffer("RECIBIDO:", buf, buflen);
+      Serial.println(DatosTipo.tipo);
+      Serial.println(DatosTipo.to);
+      Serial.println(DatosTipo.from);
+      Serial.println(DatosTipo.maxnode);
+      Serial.println(DatosTipo.hope);
+      Serial.println(DatosTipo.dato);
+      Serial.println(DatosTipo.contador);
+      Serial.println("***");
+    }
+
+    delay(200); //espero para que todos pasen al modo recepcion
+
+    if (DatosTipo.hope == MAESTRO && DatosTipo.to == MAESTRO) // Destinatario ok
+    {
+      if (DatosTipo.tipo == 2) //RESPUESTA
+      {
+        if (muestra)
+        {
+          //Computar cuanto dura el proceso
+          sprintf(tx_buf, "RESP desde #%d. Demora=%d", DatosTipo.from, millis() - DatosTipo.contador);
+          Serial.println((char *)tx_buf);
+        }
+        
+        salida += (String)DatosTipo.from + "," + (String)DatosTipo.dato + ",";
+
+        if (muestra)
+          Serial.println(salida);
+      }
+      else if (DatosTipo.tipo == 4) //PONG
+      {
+        //Computar cuanto dura el proceso
+        sprintf(tx_buf, "PONG desde #%d. Demora=%d", DatosTipo.from, millis() - DatosTipo.contador );
+        Serial.println((char *)tx_buf);
+      }
+      else if (DatosTipo.tipo == 3) //PING
+      {
+        sprintf(tx_buf, "PING desde #%d", DatosTipo.from);
+        Serial.println((char *)tx_buf);
+        DatosTipo.tipo = 4; //Retorno un PONG
+        //Permuto destino
+        DatosTipo.to = DatosTipo.from;
+        SendDatos();
+
+      }
+    }
+  }
+
+  if (timersend.onExpired()) {
+    // code only runs once per event
+    if (dump) {
+      Serial.println("Timer Send");
+    }
+    timer.restart();
+  }
+
+  if (timer.onRestart()) {
+    // Envia mensaje de interrogacion
+    DatosTipo.tipo = 1;
+    DatosTipo.to = esclavo;
+    DatosTipo.contador = millis();
+    
+    SendDatos();
+
+    if (muestra) {
+      Serial.println("-> " + (String)esclavo);
+    }
+
+    esclavo++;
+
+    if (esclavo > maxesclavo) {
+      esclavo = 1;
+      timer.stop();
+      timersend.restart();
+      if (salida != "") {
+        salida.setCharAt(salida.length() - 1, ' ');
+        Serial.println(salida); //Envia datos al GATEWAY
+        salida = "";
+      } else {
+        if (dump) {
+          Serial.println(F("Sin respuesta"));
+        }
+      }
+    }
+  }
+}
+
+void SendDatos()
+{
+  DatosTipo.hope = 1;
+  DatosTipo.maxnode = maxesclavo;
+  DatosTipo.from = MAESTRO;
+  DatosTipo.dato = 0.0;
+  //DatosTipo.contador = millis();
+
+  if (dump)
+  {
+    sprintf(tx_buf, "ENVIA MENSAJE tipo=%d destino=%d via=%d", DatosTipo.tipo, DatosTipo.to, DatosTipo.hope);
+    Serial.println((char *)tx_buf);
+
+  }
+
+  memcpy(tx_buf, &DatosTipo, sizeof(DatosTipo) );
+  driver.send(tx_buf, sizeof(DatosTipo));
+  driver.waitPacketSent();
+}
+
---
+++ MAESTRO-ESCLAVO/RH_SLAVE_TEST_V3/RH_SLAVE_TEST_V3.ino
@@ -0,0 +1,229 @@
+// RHSLAVE
+// -*- mode: C++ -*-
+// Simple example of how to use RadioHead to receive messages
+// with a simple ASK transmitter in a very simple way.
+// Implements a simplex (one-way) receiver with an Rx-B1 module
+
+#include <RH_ASK.h>
+#include <SPI.h> // Not actualy used but needed to compile
+
+#define MIID 2 //CAMBIAR SEGUN ASIGNACION DE DIRECCION
+
+#define MAESTRO 0
+
+RH_ASK driver(2400, 5, 4);
+
+struct dataStruct {
+  byte tipo; //1=interroga 2=responde 3= ping 4=pong
+  byte to;
+  byte from;
+  byte maxnode;
+  byte hope;
+  float dato;
+  unsigned long contador;
+} DatosTipo;
+
+byte buf[RH_ASK_MAX_MESSAGE_LEN];
+byte buflen = sizeof(buf);
+byte tx_buf[RH_ASK_MAX_MESSAGE_LEN + 100] = {0};
+boolean dump = true;
+unsigned long tiempo = 0;
+
+void setup()
+{
+  Serial.begin(115200);   // Debugging only
+
+  Serial.print("RH SLAVE #"); Serial.println(MIID);
+  //  Serial.println(RH_ASK_MAX_MESSAGE_LEN);
+
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+
+  if (!driver.init())
+  {
+    Serial.println("FALLA INICIO DRIVER");
+    digitalWrite(13, HIGH);
+  }
+}
+
+void loop()
+{
+  if ( Serial.available()) {
+
+    char ch = Serial.read();
+    switch (ch) {
+      //    case '0'...'9':
+      //      //v = v * 10 + ch - '0';
+      //      v = ch - '0';
+      //      break;
+      case 'd': //vuelco
+        {
+          dump = !dump;
+          if (dump)
+            Serial.println(F("VUELCO"));
+          else
+            Serial.println(F("SIN VUELCO"));
+          break;
+        }
+
+      case 'p': //transmite PING
+        {
+          int destino = Serial.parseInt();
+          DatosTipo.tipo = 3;
+          DatosTipo.to = (byte)destino;
+          DatosTipo.from = MIID;
+          if (destino > MIID)
+          {
+            DatosTipo.hope = MIID + 1;
+          }
+          else
+          {
+            DatosTipo.hope = MIID - 1;
+          }
+          DatosTipo.contador = millis();
+          tiempo = millis();
+          SendDatos();
+          break;
+        }
+    }// swith
+  }
+
+  buflen = RH_ASK_MAX_MESSAGE_LEN;
+  
+  if (driver.recv(buf, &buflen)) // Non-blocking
+  {
+    // Mensaje con checksum recibido.
+    memcpy(&DatosTipo, buf, sizeof(DatosTipo));
+
+    if (dump) {
+      driver.printBuffer("RECIBIDO:", buf, buflen);
+    }
+
+    delay(200); //espero para que todos pasen al modo recepcion
+
+    /*
+       Verifica tipo de estructura ruta y destinatario final.
+    */
+
+    if (DatosTipo.hope == MIID) //HOPE
+    {
+      if (DatosTipo.to == MIID ) //PARA MI
+      {
+
+        if (DatosTipo.tipo == 1) //INTERROGA
+        {
+          if (dump)
+            Serial.println(F("INGRESA INTERROGA - ENVIA RESPUESTA"));
+
+          //INTERROGA DESTINO FINAL -> para mi
+
+          SendMISDatos(); //envia mis datos a nodo (hope) anterior
+
+        }//INTERROGA
+
+        if (DatosTipo.tipo == 3) //INGRESA PING
+        {
+          if (dump)
+            Serial.println(F("INGRESA PING - RETORNA PONG"));
+          DatosTipo.tipo = 4; //Retorno un PONG
+          //Permuto destino
+          DatosTipo.to = DatosTipo.from;
+          //RUTEA PING
+          if (DatosTipo.to > MIID)
+          {
+            DatosTipo.hope = MIID + 1;
+          }
+          else
+          {
+            DatosTipo.hope = MIID - 1;
+          }
+          //Ajusto origen
+          DatosTipo.from = MIID;
+
+          SendDatos();
+
+        }//PING
+
+        if (DatosTipo.tipo == 4)  //INGRESA PONG
+        {
+          if (dump)
+            Serial.println(F("INGRESA PONG"));
+          //Computar cuanto dura el proceso
+          sprintf(tx_buf, "PONG desde #%d. Demora=%d", DatosTipo.from, millis() - DatosTipo.contador);
+          Serial.println((char *)tx_buf);
+        }//PONG
+
+      }//PARA MI
+      else //RUTEO
+      {
+
+        if (DatosTipo.tipo == 1) //RUTEA INTERROGA
+        {
+          if (dump)
+            Serial.println(F("RUTEA INTERROGA"));
+          DatosTipo.hope += 1;
+          SendDatos();
+        }//RUTEA INTERROGA
+
+        if (DatosTipo.tipo == 2) //RUTEA RESPUESTA
+        {
+          if (dump)
+            Serial.println(F("RUTEA RESPUESTA"));
+          DatosTipo.hope -= 1;
+          SendDatos();
+        }//RUTEA RESPUESTA
+
+        if (DatosTipo.tipo == 3 || DatosTipo.tipo == 4) //RUTEA PINGPONG
+        {
+          if (dump)
+            Serial.println(F("RUTEA PINGPONG"));
+          if (DatosTipo.to > MIID)
+          {
+            DatosTipo.hope = MIID + 1;
+          }
+          else
+          {
+            DatosTipo.hope = MIID - 1;
+          }
+
+          SendDatos(); //RUTEA PINGPONG
+        }//RUTEA PINGPONG
+
+      }//RUTEO
+    }//HOPE
+  }//RECV
+}//LOOP
+/*
+   RUTINAS
+*/
+void SendMISDatos() {
+
+  DatosTipo.tipo = 2;
+  DatosTipo.to = MAESTRO;
+  DatosTipo.from = MIID;
+  DatosTipo.hope -= 1;
+
+  int digi = analogRead(A0);
+
+  DatosTipo.dato = (float)(digi * 5.0 / 1023.0);
+//  DatosTipo.contador = millis();
+
+  //  memcpy(tx_buf, &DatosTipo, sizeof(DatosTipo));
+  //  driver.send(tx_buf, sizeof(DatosTipo));
+  //  driver.waitPacketSent();
+  //  Serial.print(F("Respuesta al MAESTRO via # ")); Serial.println(DatosTipo.hope);
+
+  SendDatos();
+}
+
+void SendDatos() {
+
+  sprintf(tx_buf, "ENVIA MENSAJE tipo=%d destino=%d via=%d", DatosTipo.tipo, DatosTipo.to, DatosTipo.hope);
+  Serial.println((char *)tx_buf);
+
+  memcpy(tx_buf, &DatosTipo, sizeof(DatosTipo));
+  driver.send(tx_buf, sizeof(DatosTipo));
+  driver.waitPacketSent();
+
+}
+
